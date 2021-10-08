da4ab4c04be63893b8ce3961b23862cce4444474
Jonatan Daniel Romero Galindo <89162105+JDanielRG@users.noreply.github.com>
2021-10-08 16:47:22 -0500

    Delete views directory

--- views/modules/errorintentos.php
+++
@@ -1,7 +0,0 @@
-<div>
-	<h1>Estimado Usuario</h1>
-</div>
-<div>
-	<p> Estimado usuario, Usted acaba de ingresar tres veces con una clave errada</p>
-	<p>Por favor pangase en contacto con el administrador del sitio</p>
-</div>
